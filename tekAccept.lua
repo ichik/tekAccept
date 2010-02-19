e6a6572c495fdc59b9ce2096464c1762646e0e8f
ichik <ichik@yandex.ru>
2010-02-19 12:48:25 +0300

    What this even did here?

---
+++ tekAccept.lua
@@ -0,0 +1,17 @@
+﻿
+local function IsFriend(name)
+	for i=1,GetNumFriends() do if GetFriendInfo(i) == name then return true end end
+	if IsInGuild() then for i=1, GetNumGuildMembers() do if GetGuildRosterInfo(i) == name then return true end end end
+end
+
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PARTY_INVITE_REQUEST")
+f:SetScript("OnEvent", function(frame, event, name)
+	if IsFriend(name) then
+		for i=1,STATICPOPUP_NUMDIALOGS do
+			local frame = getglobal("StaticPopup"..i)
+			if frame:IsVisible() and frame.which == "PARTY_INVITE" then StaticPopup_OnClick(frame, 1) end
+		end
+	else SendWho(string.join("", "n-\"", name, "\"")) end
+end)
